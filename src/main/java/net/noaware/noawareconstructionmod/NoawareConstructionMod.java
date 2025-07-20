a6be49d09b00ba7725806df7f4b86b2869588e20
noaware8966 <andreyka.ryabtsev.0023@mail.ru>
2025-07-20 20:57:08 +0300

    added important comment

--- src/main/java/net/noaware/noawareconstructionmod/NoawareConstructionMod.java
+++ src/main/java/net/noaware/noawareconstructionmod/NoawareConstructionMod.java
@@ -1,15 +1,15 @@
 package net.noaware.noawareconstructionmod;
 
 import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// Very important comment
 public class NoawareConstructionMod implements ModInitializer {
-	public static final String MOD_ID = "noawareconstructionmod";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final String MOD_ID = "noawareconstructionmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-	}
+    @Override
+    public void onInitialize() {
+    }
 }
